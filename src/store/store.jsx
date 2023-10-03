e9c226e9ee40c6128e167875070fa7397d3a88f4
Varun S P <spvarun47@gmail.com>
2023-10-04 00:05:05 +0530

    Update store.jsx

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,11 @@
-// store.js
+
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from '../reducers/rootReducer';
-// import searchReducer from '../reducers/searchSlice'
+
 
 const store = configureStore({
   reducer: rootReducer,
-  // ...other store configurations
+
 });
 
 export default store;

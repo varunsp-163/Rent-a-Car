05c023014a1e88efc50cb98f16249629f02fe44d
Varun S P <spvarun47@gmail.com>
2023-10-04 00:05:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom";
-import { Provider } from "react-redux"; // Import Provider from react-redux
-import store from "./store/store"; // Import your Redux store
+import { Provider } from "react-redux"; 
+import store from "./store/store";
 import "./index.css";
 import App from "./App";
 const root = createRoot(document.getElementById("root"));
